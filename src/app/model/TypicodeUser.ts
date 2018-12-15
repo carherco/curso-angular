98c7ce5e27acd8bbdffe9092dc0d076519b991ef
Carlos Herrera Conejero <carherco@gmail.com>
2018-12-15 18:40:21 +0100

    archivo con usuarios

--- src/app/model/TypicodeUser.ts
+++ src/app/model/TypicodeUser.ts
@@ -1,11 +1,15 @@
+import { Company } from './company';
 import { Address } from "./address";
 
 export class User {
   constructor(
-    public id: number,
-    public name: string,
-    public username: string,
-    public email: string,
-    public address: Address
+    public id?: number,
+    public name?: string,
+    public username?: string,
+    public email?: string,
+    public address?: Address,
+    public phone?: string,
+    public website?: string,
+    public company?: Company
   ){}
 }
---
+++ src/app/model/company.ts
@@ -0,0 +1,7 @@
+export class Company {
+  constructor(
+    public name?: string,
+    public catchPhrase?: string,
+    public bs?: string
+  ) {}
+}
